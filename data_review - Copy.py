06230b6e77f755c7347ff19e6e02bfa09cfa93b7
Eric <hxleee@gmail.com>
2018-03-21 16:15:03 +0800

    add new file
    sdfa

---
+++ data_review - Copy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 21 14:50:03 2018
+
+@author: eric.li
+"""
+
+import pandas as pd
+
+train = pd.read_csv("../input/train_sample.csv.zip")
+
+train = pd.read_csv("../input/train.csv.zip")
